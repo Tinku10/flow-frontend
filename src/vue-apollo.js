cb51d53e6696aad368e8443ce5226c4c2cea1eac
Tinku Kalita <tinku.kvs@gmail.com>
2020-04-14 00:23:59 +0530

    'correcting a path error'

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -9,7 +9,7 @@ Vue.use(VueApollo)
 const AUTH_TOKEN = 'apollo-token'
 
 // Http endpoint
-const httpEndpoint = process.env.VUE_APP_GRAPHQL_HTTP || 'https://fathomless-mesa-81724.herokuapp.com//graphql'
+const httpEndpoint = process.env.VUE_APP_GRAPHQL_HTTP || 'https://fathomless-mesa-81724.herokuapp.com/graphql'
 
 // Config
 const defaultOptions = {
